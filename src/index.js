31e8f40a5644efe051a2c7787e109e8ded17b7b5
chynnalew <chynnalew@yahoo.com>
2021-09-07 14:33:29 -0700

    add drop menu for zip/city search

--- src/index.js
+++ src/index.js
@@ -3,27 +3,36 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
+function kelvinToF(k){
+  return ((k - 273.15) * 9/5 + 32);
+}
+
 $(document).ready(function() {
   $('#weatherLocation').click(function() {
-    const city = $('#location').val();
+    const location = $('#location').val();
     $('#location').val("");
 
     let request = new XMLHttpRequest();
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
+    const url = `http://api.openweathermap.org/data/2.5/weather?${$("#input").val()}=${location}&appid=${process.env.API_KEY}`;
 
     request.onreadystatechange = function() {
       if (this.readyState === 4 && this.status === 200) {
         const response = JSON.parse(this.responseText);
         getElements(response);
+      } else if (this.readyState === 4 && this.status === 404) {
+        alert('City not found!');
       }
-    };
+    }
 
     request.open("GET", url, true);
     request.send();
 
-  function getElements(response) {
-      $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-      $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+    function getElements(response) {
+      $('.showHumidity').text(`The humidity in the place is ${response.main.humidity}%`);
+      $('.showTemp').text(`The temperature in F is function ${kelvinToF(response.main.temp)} degrees.`);
+      $('.cloudCover').text(`The clouds are ${response.weather[0].description}.`);
+      $('.windSpeed').text(`The wind speed is ${response.wind.speed}.`);
+      $('.showName').text(`${response.name}`);
     }
   });
 });
